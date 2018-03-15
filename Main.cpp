77ece5c332d57297fcb3d095f5f1f4d0e2bb5531
EthanReese <ethan.reese01@gmail.com>
2018-03-15 10:44:50 -0700

    Made the general structure and the input system work

--- Main.cpp
+++ Main.cpp
@@ -6,4 +6,106 @@
 #include <cstdlib>
 #include <iostream>
 
+struct Node{
+     int data;
+     Node* left;
+     Node* right;
+};
+
+void addTree(int element, Node* head);
+void printTree(Node* head);
+bool deleteHeap(Node* head);
+
 using namespace std;
+
+int main(){
+     bool going = true;
+     //Keep getting inputs until the user asks to stop
+     int counter = 0;
+     char* input;
+     Node* head = NULL;
+     cout << "Enter the character I if you would like to be prompted for input: " << endl;
+     char* input_1;
+     input_1 = new char[80];
+     cin >> input_1;
+     if(strcmp(input_1, "I") == 0){
+          cout << "Enter your numbers and enter the character N when done." << endl;
+             while(going){
+                  cout << "Number: ";
+                  input = new char[80];
+                  cin >> input;
+                  //If the user inputs the N then it needs to stop
+                  if(strcmp(input, "N") == 0){
+                       going = false;
+                       delete input;
+                       printTree(head);
+                  }
+                  else if(counter == 100){
+                       going = false;
+                       delete input;
+                       printTree(head);
+                  }
+                  else if(strcmp(input, "0") == 0){
+                       //If its zero is screws things up so just ignore it since its not in the range anyway.
+                  }
+                  else{
+                          //put the input into a string stream to convert it into aninteger for comparison
+                          stringstream convert;
+                          convert << input;
+                          int element = 0;
+                          convert >> element;
+                          counter++;
+                          delete input;
+                          addHeap(element, heap);
+                  }
+             }
+     }
+     else{
+          cout << "Please enter the filename of the file you would like to read in: ";
+          input = new char[1000];
+          cin >> input;
+          //Create a stream to open the file
+          ifstream inFile;
+          inFile.open(input);
+          if(!inFile){
+               return 1;
+          }
+          int x;
+          while(inFile >> x){
+               addTree(x, head);
+          }
+          printTree(head);
+          delete input;
+     }
+         cout << "Enter any integers that you would like to delete from the tree and type 'done' to quit and 'print' to print";
+     while(true){
+          cout << "Command: ";
+          char* input = new char[80];
+          cin >> input;
+          //If the user wants to quit let them
+          if(strcmp(input, "done") == 0){
+               return 1;
+               delete input_1;
+          }
+          else if(strcmp(input, "print") == 0){
+               printTree(head);
+          }
+          else{
+               stringstream convert;
+               convert << input;
+               int number = 0;
+               convert >> number;
+               delete input;
+               //If the number is in the tree then we're all good, but if it isn't give the user a heads up
+               if(!deleteTree(number, head){
+                    cout << "That value is not in the tree" << endl;
+               }
+          }
+     }
+}
+
+//Add an input element into the tree
+void addTree(int element, Node* head){
+     Node* current = head;
+
+}
